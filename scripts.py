007a076d5d16e0f1963a8ec64f60cf108497d93c
Stmkv <stepkamakarov1999@mail.ru>
2024-09-11 22:16:27 +0300

    Починил баг с склонениями

--- scripts.py
+++ scripts.py
@@ -1,35 +1,34 @@
-import pandas as pd
 import pprint
-
 from collections import defaultdict
+
+import pandas as pd
 
 
 def get_format_year(year: int) -> str:
-    last_num = int(str(year)[-1])
-    if 11 <= year <= 2:
-        return f"лет"
-    elif last_num == 1:
-        return f"год"
-    elif 2 <= last_num <= 4:
-        return f"года"
+    last_two_nums = int(str(year)[-2:])
+    if 11 <= last_two_nums <= 19:
+        return "лет"
+    elif last_two_nums % 10 == 1:
+        return "год"
+    elif 2 <= last_two_nums % 10 <= 4:
+        return "года"
     else:
-        return f"лет"
+        return "лет"
 
 
 def read_exel_file(file):
-    excel_file = pd.read_excel(file, usecols=['Категория',
-                                              'Название',
-                                              'Сорт',
-                                              'Цена',
-                                              'Картинка',
-                                              'Акция'],
-                               keep_default_na=False)
+    excel_file = pd.read_excel(
+        file,
+        usecols=["Категория", "Название", "Сорт", "Цена", "Картинка", "Акция"],
+        keep_default_na=False,
+    )
     return excel_file
+
 
 def get_dict_with_alcohol(file):
     alcohols = defaultdict(list)
 
     for wine in file:
-        category = wine['Категория']
+        category = wine["Категория"]
         alcohols[category].append(wine)
     return dict(alcohols)
